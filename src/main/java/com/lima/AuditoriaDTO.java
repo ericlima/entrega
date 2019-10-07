d3aa359f41e8b68d331117e9d17b24d7b0125aed
eric <eric.george.lima@gmail.com>
2019-10-07 02:03:53 +0100

    inicial

---
+++ src/main/java/com/lima/AuditoriaDTO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lima;
+
+/**
+ *
+ * @author eric
+ */
+public class AuditoriaDTO  {
+
+    private Long idCliente;
+    private String operacao;
+    private String campo;
+    private String valorAnterior;
+    private String valorAtual;
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getOperacao() {
+        return operacao;
+    }
+
+    public void setOperacao(String operacao) {
+        this.operacao = operacao;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getValorAnterior() {
+        return valorAnterior;
+    }
+
+    public void setValorAnterior(String valorAnterior) {
+        this.valorAnterior = valorAnterior;
+    }
+
+    public String getValorAtual() {
+        return valorAtual;
+    }
+
+    public void setValorAtual(String valorAtual) {
+        this.valorAtual = valorAtual;
+    }
+        
+    
+    
+}
